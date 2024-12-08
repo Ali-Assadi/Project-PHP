f4a9cc480f0f0d358da64cf650514ac15eb345d1
Ali Assadi <aliahwc@gmail.com>
2024-12-08 14:00:24 +0200

    fixFooter,Add Photos

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="footer">
+<footer class="footer" style="height: 41px">
     <marquee class="footer-marquee">
         © 2024 Restaurant. All rights reserved.
     </marquee>
--- header.php
+++ header.php
@@ -4,10 +4,10 @@ $imagesPath = "photos/index_images/";
 
 <header class="header" style="background: linear-gradient(-135deg, rgb(63, 63, 63), rgb(182, 165, 132));">
     <a href="signin.php" target="main">
-        <img src="<?= $imagesPath ?>usericon.png" id="usericon"/>
+        <img src="<?= $imagesPath ?>login.png" id="usericon" style="height:50px weight:50px"/>
     </a>
     <a href="sign_Up.php" target="main">
-        <img src="<?= $imagesPath ?>usericon.png" id="usericon"/>
+        <img src="<?= $imagesPath ?>signup.png" id="usericon"/>
     </a>
     <img src="<?= $imagesPath ?>navBarBackground.png" id="left" />
     <img src="photos/logo_Images/Logo99.png" id="logo" />
