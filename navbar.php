8f791ff351386cd7d879841ca397fd6c8a6dfd8f
Ali Assadi <aliahwc@gmail.com>
2024-12-14 20:21:51 +0200

    FixingButton

--- navbar.php
+++ navbar.php
@@ -10,11 +10,11 @@
           <a href="drink.php" target="main">
             <button class="button button3">Drinks</button>
           </a><br /><br />
-          <a href="location.php" target="main">
-            <button class="button button4">Our Place</button>
-          </a><br /><br />
           <a href="reservation.php" target="main">
             <button class="button button5">Reservation</button>
+          </a><br /><br />
+          <a href="location.php" target="main">
+            <button class="button button4">Our Place</button>
           </a><br /><br />
           <a href="contact-us.php" target="main">
             <button class="button button6">Contact Us</button>
